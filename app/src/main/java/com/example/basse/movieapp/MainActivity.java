882a9c8a4bad6c85ac73f771ff56f9c77ab8c67d
Almostafa Loulah <bassem.hassan1995@gmail.com>
2016-10-02 19:06:28 +0200

    Upgrading the project version to 2.2

--- app/src/main/java/com/example/basse/movieapp/MainActivity.java
+++ app/src/main/java/com/example/basse/movieapp/MainActivity.java
@@ -19,16 +19,6 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private MainPagerAdapter mSectionsPagerAdapter;
-
-    /**
      * The {@link ViewPager} that will host the section contents.
      */
     public ViewPager mViewPager;
@@ -51,7 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
             // Create the adapter that will return a fragment for each of the three
             // primary sections of the activity.
-            mSectionsPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+            /*
+      The {@link android.support.v4.view.PagerAdapter} that will provide
+      fragments for each of the sections. We use a
+      {@link FragmentPagerAdapter} derivative, which will keep every
+      loaded fragment in memory. If this becomes too memory intensive, it
+      may be best to switch to a
+      {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+            MainPagerAdapter mSectionsPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
 
             // Set up the ViewPager with the sections adapter.
             mViewPager = (ViewPager) findViewById(R.id.container);
@@ -95,10 +93,9 @@ public class MainActivity extends AppCompatActivity {
      */
     public class MainPagerAdapter extends FragmentPagerAdapter {
 
-        public MainPagerAdapter(FragmentManager fm) {
+        MainPagerAdapter(FragmentManager fm) {
             super(fm);
         }
-
 
         @Override
         public int getCount() {
